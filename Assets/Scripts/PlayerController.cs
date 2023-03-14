273b65619377e1facb4d785040c911dbb4d589da
carterweldon <cawe4a6@student.oaisd.org>
2023-03-14 14:31:53 -0400

    Waoha

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 5f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 10f;
+
+
+    Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+     void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
